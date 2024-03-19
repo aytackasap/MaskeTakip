fe3b0fa319bb1af7cf9c677862fc41f56f884984
Aytac Kasap <ulukasap@gmail.com>
2024-03-19 03:38:12 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,29 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Person person1 = new Person();
+            person1.FirstName = "ENGİN";
+            person1.LastName = "DEMİROĞ";
+            person1.DateOfBirthYear = 1985;
+            person1.NationalIdentity = 1;
+
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+        }
+
+
+
+
+    }
+}
+
